e2b33f37c0cf5e7dbe46815124d3a6932d97a694
Vkirovpz <Vkirovpz@students.softuni.bg>
2025-02-19 23:26:41 +0200

    Add Book model and controller

---
+++ Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
